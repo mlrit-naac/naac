cf182b0d1321328e87fa57af33741362deadc285
dinesh-p0 <dineshkrishna0456@gmail.com>
2023-10-30 21:33:26 +0530

    firsc

---
+++ components/Lay.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Lay = () => {
+  return (
+    <div className='bg-[#dcf3da] w-full flex '>
+        <div className='bg-[#129104]  w-[200px] flex flex-col items-center h-[400px] '>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p className=''>Home</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-transparent'>
+                <p>Extended Profile</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 1</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 2</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 3</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 4</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 5</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 6</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 7</p>
+            </div>
+            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <p>Criterion - 8</p>
+            </div>
+            {/* <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+                <a href="/icw1a9.pdf">Helllo</a>
+            </div> */}
+
+            {/* <div className='py-2 w-[150px] pl-4 hover:bg-black'>
+            <a href="/icw1a9.pdf" target="_blank" rel="noopener noreferrer">Download FIle</a>
+            </div> */}
+
+
+        </div>
+        <div className='py-[40px] w-full  px-[40px]'>
+            <div className=' bg-[#dcf3da]  min-h-screen flex flex-col justify-start'>
+                <div className='flex flex-row justify-center items-center bg-[#2196f3] py-2'>
+                    <p className=' text-4xl'>Criterion 1 - Curricular Aspects</p>
+                </div>
+                
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Lay
