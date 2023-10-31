a5f59f50fa0d95b5d04d18ce14b5839d5d1ed967
dinesh-p0 <dineshkrishna0456@gmail.com>
2023-10-31 16:09:59 +0530

    criterion1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Lay from '@/components/Lay'
+import Criterion1 from '@/components/Criterion1'
 import Nav from '@/components/Nav'
 const page = () => {
   return (
-    <div className='h-[200vh] bg-[#dcf3da]'>
+    <div className='bg-[#dcf3da]'>
       <header className='w-full h-[100px] flex flex-row  bg-white'>
         <img src="/mlrit.jpeg" alt="mlrit" className='h-[100px]'/>
         <div className='flex flex-row justify-center w-full'>
@@ -11,7 +11,7 @@ const page = () => {
         </div>
       </header>
       <Nav />
-      <Lay />
+      <Criterion1 />
 
     </div>
   )
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,7 +13,7 @@ const Nav = () => {
             <span className="mr-1">Extended Profile</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10  justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">Programs</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">students</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">Teachers</a></li>
@@ -25,7 +25,7 @@ const Nav = () => {
             <span className="mr-1">Criterion - 1</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10  justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">1.1</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">1.2</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">1.3</a></li>
@@ -37,7 +37,7 @@ const Nav = () => {
             <span className="mr-1">Criterion - 2</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10 justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">2.1</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">2.2</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">2.3</a></li>
@@ -52,7 +52,7 @@ const Nav = () => {
             <span className="mr-1">Criterion - 3</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10  justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">3.1</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">3.2</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">3.3</a></li>
@@ -67,7 +67,7 @@ const Nav = () => {
             <span className="mr-1">Criterion - 4</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10 justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">4.1</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">4.2</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">4.3</a></li>
@@ -79,7 +79,7 @@ const Nav = () => {
             <span className="mr-1">Criterion - 5</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10 justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">5.1</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">5.2</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">5.3</a></li>
@@ -91,7 +91,7 @@ const Nav = () => {
             <span className="mr-1">Criterion - 6</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10 justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">6.1</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">6.2</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">6.3</a></li>
@@ -105,7 +105,7 @@ const Nav = () => {
             <span className="mr-1">Criterion - 7</span>
             <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
             </button>
-            <ul className="dropdwn-menu absolute w-full  justify-center items-center hidden  text-gray-700 bg-slate-200">
+            <ul className="dropdwn-menu absolute w-full z-10 justify-center items-center hidden  text-gray-700 bg-slate-200">
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200    px-4 py-2 hover:bg-gray-400" href="#">7.1</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">7.2</a></li>
                 <li className=""><a className="whitespace-no-wrap block bg-gray-200 px-4 py-2 hover:bg-gray-400" href="#">7.3</a></li>
