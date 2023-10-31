55743e093fb873d3f067484046e32516015ca2ab
dinesh-p0 <dineshkrishna0456@gmail.com>
2023-10-31 14:36:05 +0530

    newnav

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Lay from '@/components/Lay'
+import Nav from '@/components/Nav'
 const page = () => {
   return (
-    <div>
-      <header className='w-full h-[100px] bg-[#dcf3da]'>
+    <div className='h-[200vh] bg-[#dcf3da]'>
+      <header className='w-full h-[100px] flex flex-row  bg-white'>
         <img src="/mlrit.jpeg" alt="mlrit" className='h-[100px]'/>
+        <div className='flex flex-row justify-center w-full'>
+          <img src="https://mlritnaac.web.app/images/header_cut.png" alt="" />
+        </div>
       </header>
+      <Nav />
       <Lay />
+
     </div>
   )
 }
--- components/Lay.tsx
+++ components/Lay.tsx
@@ -3,53 +3,59 @@ import React from 'react'
 const Lay = () => {
   return (
     <div className='bg-[#dcf3da] w-full flex '>
-        <div className='bg-[#129104]  w-[200px] flex flex-col items-center h-[400px] '>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p className=''>Home</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-transparent'>
-                <p>Extended Profile</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 1</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 2</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 3</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 4</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 5</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 6</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 7</p>
-            </div>
-            <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <p>Criterion - 8</p>
-            </div>
-            {/* <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-                <a href="/icw1a9.pdf">Helllo</a>
-            </div> */}
-
-            {/* <div className='py-2 w-[150px] pl-4 hover:bg-black'>
-            <a href="/icw1a9.pdf" target="_blank" rel="noopener noreferrer">Download FIle</a>
-            </div> */}
-
-
-        </div>
-        <div className='py-[40px] w-full  px-[40px]'>
-            <div className=' bg-[#dcf3da]  min-h-screen flex flex-col justify-start'>
+        <div className='py-[8px]  bg-[#dcf3da]  w-full  px-[20px]'>
+            <div className='  min-h-screen flex flex-col justify-start'>
                 <div className='flex flex-row justify-center items-center bg-[#2196f3] py-2'>
                     <p className=' text-4xl'>Criterion 1 - Curricular Aspects</p>
                 </div>
-                
+                <div className='p-8 border-b-2 border-gray-400'>
+                  <p className='text-2xl text-center text-red-500'>Key Indicator - 1.1 Curriculum Design and Development</p>
+                </div>
+                <div className='flex flex-row py-8 px-4 border-b-2 border-gray-400'>
+                  <div className='px-8'><p className='text-left font-bold text-lg text-black'>1.1.1</p></div>
+                  <div>
+                    <p className='text-left font-bold text-lg text-black'>
+                    Curricular developed and implemented have relevance to the local, national, regional and global developmental needs which is reflected in Programme outcomes (POs), Programme Specific outcomes (PSOs) and Course Outcomes (COs) of the Programmes offered by the Institution
+                    </p>
+                    <ul>
+                      <li>
+                          <p className='text-left  text-lg text-black'>
+                          MLR Institute of Technology emphasizes on the developmental needs at the local/national/global levels with formative necessities unblemished while planning its curriculum. The learning process manoeuvres through result based instructional methods, with each course having a port of call which in turn is being driven by a well conceived mission and vision. The educational program is planned on after premise.                       </p>
+                      </li>
+                      <li>
+                          <p className='text-left  text-lg text-black'>
+                          Curriculum maintains the balance in the composition of Basic Science, Engineering Sciences, Humanities and Social Sciences, Program Core, Program Electives, Open Electives, Projects Work and Employability Enhancement. </p>
+                      </li>
+                      <li>
+                          <p className='text-left  text-lg text-black'>
+                          Program Assessment Committee (PAC) coordinator takes the inputs from Domain Experts to identify the imperative inputs received from the stakeholders and prepares the flowchart through which their inputs can be inculcated in the curriculum.
+                          </p>                      
+                      </li>
+                      <li>
+                          <p className='text-left  text-lg text-black'>
+                          Curriculum is designed in line with guidelines of the program specific criteria set-up by lead societies like American Institute of Aeronautics and Astronautics (AIAA), ASME, CSI, IEEE etc,.                          </p>                      
+                      </li>
+                      <li>
+                          <p className='text-left  text-lg text-black'>
+                          The curriculum is designed keeping the AICTE model curriculum as a base reference of UG (B.Tech) in Information Technology Program. The Choice Based Credit System (CBCS) incorporated in the Curriculum provides an opportunity for the students to choose courses from the prescribed courses comprising core, elective or skill based courses.
+                          </p>                      
+                      </li>
+                      <li>
+                          <p className='text-left  text-lg text-black'>
+                          In further improving the existing curriculum for bridging the gaps, the feedback from the students, alumni, faculty, recruiters and industry experts is collected and sorted for continuous improvement besides, the structure of the course offered by IITs and other National & International Universities is thoroughly studied.                          </p>                      
+                      </li>
+                      <li>
+                          <p className='text-left  text-lg text-black'>
+                          While designing the curriculum, the institution lays emphasis on developing conceptual skills, human skills and technical skills of the individual. It is ensured that the overall curriculum and its components contribute effectively in the attainment of POs, PSOs and PEOs which in turn relate to the Vision and Mission of the Department and institute as well.  The initial version of the curriculum is prepared by having the above design criteria, through discussions with stakeholders. The proposed curriculum is then discussed in DAC (Department Academic Committee) and is put forth to BOS (Board of Studies) where the experts from industry, academia, Alumni in addition to senior faculty members. The curriculum is then placed for approval during the Academic Council (AC). Curriculum is finally evolved and published in MLR Institute of Technology.                          </p>                      
+                      </li>
+                      <div className='bg-[#40a33e] w-full p-4 mt-4 hover:bg-slate-300 '>
+                        <a href="/1.1.1_claim_form.pdf " className='w-full  text-left' target="_blank" rel="noopener noreferrer">Claim Form</a>
+                      </div>
+
+                    </ul>
+                  </div>
+                </div>
+              
             </div>
 
         </div>
