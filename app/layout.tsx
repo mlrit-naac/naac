e2f413510f9d9eb1a696615a934d4b755cc292bc
dinesh-p0 <dineshkrishna0456@gmail.com>
2023-11-14 13:45:35 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       
 
      
-       <header className='w-full h-[100px] fixed top-0 flex flex-row  bg-white'>
+       <header className='w-full h-[100px] fixed top-0 flex flex-row  bg-[#dcf3da] '>
         <img src="/mlrit.jpeg" alt="mlrit" className='h-[100px]'/>
         <div className='flex flex-row justify-center w-full'>
           <img src="https://mlritnaac.web.app/images/header_cut.png" alt="" />
@@ -33,7 +33,7 @@ export default function RootLayout({
        </header>
        <div className='flex flex-row w-full'>
         <nav className='bg-[#129104] hidden lg:flex overflow-y-auto fixed  top-[100px] left-0'>
-          <div className='flex flex-col h-[99vh] z-1 w-[300px]'>
+          <div className='flex flex-col h-[98vh] overflow-y-auto z-1 w-[300px]'>
             
             <div className='p-4'>
               <a href='/'>Home</a>
@@ -61,7 +61,7 @@ export default function RootLayout({
               {crt2.map(function(data){
                 return(
                   <div>
-                  {/* <a href='/criterion1/1.2' className='px-4 border-l-2  border-stone-300 hover:border-white active:border-red-600'>{data}</a> */}
+                  <a href='/criterion1/1.2' className='px-4 border-l-2  border-stone-300 hover:border-white active:border-red-600'>{data.n}</a>
 
                   </div>
                 )
@@ -71,30 +71,15 @@ export default function RootLayout({
               <p>Criterion-3</p>
             </div>
             <div className='px-6 flex flex-col gap-1'>
-            <a href='/criterion1/1.1' className='px-4 border-l-2  border-stone-300 hover:border-white active:border-red-600'>1.1</a>
-              <a href='/criterion1/1.2' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.2</a>
-              <a href='/criterion1/1.3' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.3</a>
-              <a href='/criterion1/1.4' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.4</a>
-              <a href='/criterion1/1.1' className='px-4 border-l-2  border-stone-300 hover:border-white active:border-red-600'>1.1</a>
-              <a href='/criterion1/1.2' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.2</a>
-              <a href='/criterion1/1.3' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.3</a>
-              <a href='/criterion1/1.4' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.4</a>
-              <a href='/criterion1/1.2' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.2</a>
-              <a href='/criterion1/1.3' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.3</a>
-              <a href='/criterion1/1.4' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.4</a>
-              <a href='/criterion1/1.1' className='px-4 border-l-2  border-stone-300 hover:border-white active:border-red-600'>1.1</a>
-              <a href='/criterion1/1.2' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.2</a>
-              <a href='/criterion1/1.3' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.3</a>
-              <a href='/criterion1/1.4' className='px-4 border-l-2 border-stone-300 hover:border-white active:border-red-600'>1.4</a>
-
-              {/* {crt3.map(function(data){
+            
+              {crt3.map(function(data){
                 return(
                   <div>
-                  <a href='/criterion1/1.2' className='px-4 border-l-2  border-stone-300 hover:border-white active:border-red-600'>{data.n}</a>
+                  <a href='/criterion1/1.2' className='px-4 border-l-2  border-stone-300 hover:border-white active:border-red-600'>{data}</a>
 
                   </div>
                 )
-              })} */}
+              })}
                                 
             </div>
           </div>
