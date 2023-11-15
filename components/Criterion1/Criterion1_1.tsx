cc9e9c19017e0453aa93b6a5ff65562c36df4889
Abhiram <vemulaabhiram2004@gmail.com>
2023-11-15 14:40:01 +0530

    claimform1.1.1

--- components/Criterion1/Criterion1_1.tsx
+++ components/Criterion1/Criterion1_1.tsx
@@ -106,7 +106,7 @@ const Criterion1 = () => {
                   </p>
                 </li>
                 <a
-                  href="http://localhost:3000/1.1.1_claim_form.pdf "
+                  href="/criterion1/1.1/1.1.1/claimform.pdf"
                   className="w-full  text-left flex"
                   target="_blank"
                   rel="noopener noreferrer"
